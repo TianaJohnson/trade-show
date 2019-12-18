f48109f255f6ff0ca7dbfb147b525b498d1d3c33
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-17 19:19:45 -0600

    there has got to be a better way to create the margin between the card/background/nav withought using a br

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 //import { connect } from 'react-redux';
 import UserHeader from '../UserHeader/UserHeader';
+import Card from '@material-ui/core/Card';
+import './Dashboard.css';
 
 class Dashboard extends Component {
 
     render(){
         return (
-            <div>
-                <UserHeader/>
+            <div className="dashboard">
+                <br/>
+                <Card className="dash-card">
+                    <UserHeader/>
+                </Card>
                 <br/>
             </div>
         )
