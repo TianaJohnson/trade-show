3f965503160b27d26875267323493f53ef1ab13c
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-18 20:38:34 -0600

    add new

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-class Template extends Component {
+class AddNew extends Component {
     render() {
         return (
             <div></div>
