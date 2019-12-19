cb30f19cfc26683bcc91ef6927919396c5c61432
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-18 20:37:42 -0600

    snippets are cool

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 class Template extends Component {
-render() {
-return (
-<div></div>
- )
-}
+    render() {
+        return (
+            <div></div>
+         )
+    }
 }
 const mapStateToProps = reduxStore => {
-return { reduxStore: reduxStore };
-}
-export default connect(mapStateToProps)($(Template));
+    return { reduxStore: reduxStore };
+    }
+export default connect(mapStateToProps)(Template);
