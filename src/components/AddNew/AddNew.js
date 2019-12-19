29981170af14eac99ae2cb00cc43f4218194890a
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-18 20:47:21 -0600

    trump has been impeached

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
 
 class AddNew extends Component {
     render() {
         return (
             <div>
                 <Card className="addNewCard">
-
+                    <TextField id="outlined-basic" 
+                               label="Outlined" 
+                               variant="outlined">
+                    </TextField>
                 </Card>
             </div>
          )
