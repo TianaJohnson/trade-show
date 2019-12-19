353bb5618e38a8f65bbc07abd32361c5b755ce97
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-18 20:43:44 -0600

    add new card

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Card from '@material-ui/core/Card';
+
 class AddNew extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <Card className="addNewCard">
+
+                </Card>
+            </div>
          )
     }
 }
