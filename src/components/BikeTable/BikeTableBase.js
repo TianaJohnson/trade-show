5db93bae9e16012d570564ca7c8a3700602af3d8
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-20 10:04:16 -0600

    ...

--- src/components/BikeTable/BikeTableBase.js
+++ src/components/BikeTable/BikeTableBase.js
@@ -1,44 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import './BikeFile.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import "./BikeFile.css";
 
 class BikeTableBase extends Component {
-    render() {
-        return (
-            <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-    }
+  render() {
+    return (
+      <Paper>
+        <Table className="admin_table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Brand</TableCell>
+              <TableCell>Builder</TableCell>
+              <TableCell>Edit Customer Info</TableCell>
+              <TableCell>View/Edit</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          {/* <TableBody>
+         {this.props.reduxStore.addCust.customerReducer.map(client =>
+           <AdminRow key={client.id} history={this.props.history} client={client}/>
+         )}
+         
+       
+   </TableBody> */}
+        </Table>
+      </Paper>
+    );
+  }
 }
 const mapStateToProps = reduxStore => {
-    return { reduxStore: reduxStore };
-    }
+  return { reduxStore: reduxStore };
+};
 export default connect(mapStateToProps)(BikeTableBase);
