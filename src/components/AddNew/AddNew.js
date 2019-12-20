7c2048094410d3ecc487769a2cd98077672f4d11
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-19 19:39:25 -0600

    button for build'

--- src/components/AddNew/AddNew.js
+++ src/components/AddNew/AddNew.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import './AddNew.css';
 
 class AddNew extends Component {
@@ -16,6 +17,13 @@ class AddNew extends Component {
                                label="Outlined" 
                                variant="outlined">
                     </TextField>
+                    <br/>
+                    <Button variant="contained" 
+                            color="secondary"
+                            //onClick={}
+                            style={{ margin: 10 }}>
+                    Build!
+                    </Button>
                     </div>
                 </Card>
             </div>
