e7dcc40cab26b6c03417205630877e0855c1d766
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-08 17:32:09 -0600

    table row build

--- src/components/BikeTable/BikeTableRow.js
+++ src/components/BikeTable/BikeTableRow.js
@@ -6,9 +6,32 @@ import './BikeTable.css';
 class BikeTableRow extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <TableRow>
+            <TableCell>{this.props.client.customers_full_name}</TableCell>
+            <TableCell>{this.props.client.phone_number}</TableCell>
+            <TableCell> <Button variant="contained"
+                size="small"
+                onClick={this.editCust}
+                color="primary"
+                style={{ margin: 10 }}>
+                Edit
+            </Button></TableCell>
+            <TableCell><Button variant="contained"
+                    color="secondary"
+                    onClick={this.custFile}
+                    style={{ margin: 10 }}>
+                    Customer File
+            </Button></TableCell>
+            <TableCell> 
+                <Button variant="contained"
+                    color="inherit"
+                    onClick={this.archiveCust}
+                    style={{ margin: 10 }}>
+                    Delete
+                </Button>
+            </TableCell>
+
+        </TableRow>
          )
     }
 }
