84295acdeb90c23524b4c331f4fcf067fab8f516
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-16 10:08:23 -0600

    redesigne, creating a main page that will access all shows

---
+++ src/components/MainPage/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import './Main.css';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main-div">
+                <Card className="main-card">
+
+                </Card>
+            </div>
+         )
+    }
+}
+const mapStateToProps = reduxStore => {
+    return { reduxStore: reduxStore };
+    }
+export default connect(mapStateToProps)(Main);
