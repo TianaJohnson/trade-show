c644cf9cc742691df0c37ff530910b6c51f0563b
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-16 11:19:24 -0600

    bike file build in db, incomplete

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ CREATE TABLE "user" (
 );
 
 -- Show info intake
-CREATE TABLE "initial_intake" (
+CREATE TABLE "show_intake" (
 	"id" SERIAL PRIMARY KEY,
 	"show" VARCHAR (200),
 	"location" VARCHAR (100),
@@ -16,6 +16,31 @@ CREATE TABLE "initial_intake" (
 	"city" VARCHAR (30),
 	"country" VARCHAR (40)
 	);
+
+-- builder intake
+CREATE TABLE "builder_intake" (
+	"id" SERIAL PRIMARY KEY,
+	"show_id" INT REFERENCES "show_intake",
+	"brand" VARCHAR (200),
+	"first_name" VARCHAR (100),
+	"last_name" VARCHAR (100)
+	);
+
+-- bike intake
+-- incomplete and not built in the db yet
+CREATE TABLE "bike_intake" (
+	"id" SERIAL PRIMARY KEY,
+	"builder_id" INT REFERENCES "builder_intake"("id"),
+	"bike_type" VARCHAR (100),
+	"wheel_size" VARCHAR (20),
+	"tire_size" VARCHAR (20)
+	"axle_type" VARCHAR (30),
+	"brake_type" VARCHAR (30),
+	"drive_train" VARCHAR (30),
+	"fork_build" VARCHAR (30),
+	"notes" VARCHAR (400),
+	"main_comp_brand" VARCHAR (200),
+		);
 
 
 
