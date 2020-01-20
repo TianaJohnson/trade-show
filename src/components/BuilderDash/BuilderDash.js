a877e4a69cd157e520bf086652745e71a901f6a2
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-19 18:47:13 -0600

    build dash build

--- src/components/BuilderDash/BuilderDash.js
+++ src/components/BuilderDash/BuilderDash.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 
 
 class BuilderDash extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <Card>
+                    <br/>
+                    <Button>
+
+                    </Button>
+                    <br/>
+                </Card>
+            </div>
          )
     }
 }
