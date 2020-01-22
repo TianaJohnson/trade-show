4ab474a794d5d25164f0ef9a50d388d956606e9b
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-21 22:22:12 -0600

    :

--- src/components/BuildDash/BuildDash.js
+++ src/components/BuildDash/BuildDash.js
@@ -6,14 +6,19 @@ import Card from '@material-ui/core/Card';
 
 class BuildDash extends Component {
 
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_SHOW', payload: { id: this.props.match.params.id } });    
+      }
+
     addNew = () => {
         this.props.history.push('/add');
       }
-      
+
     render() {
         return (
             <div>
                 <Card>
+                    Show:{this.props.}
                     <br/>
                     <Button variant="contained"
                     color="primary"
