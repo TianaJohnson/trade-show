ab1dd06d2de7af980c1fdb725195ad22d0c47568
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-23 12:46:11 -0600

    still searching

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -2,7 +2,7 @@ import { all } from 'redux-saga/effects';
 import loginSaga from './loginSaga';
 import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
-import add from './showIntakeSaga';
+import show from './showSaga';
 import build from './builderSaga';
 
 // some sagas trigger other sagas, as an example
@@ -13,7 +13,7 @@ export default function* rootSaga() {
     loginSaga(),
     registrationSaga(),
     userSaga(),
-    add(),
+    show(),
     build()
   ]);
 }
