c0016dfd8bbdbc64a60c780c0002c6b6d0708dd6
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-29 20:01:59 -0600

    card

--- src/components/BuildDash/BuilderTable.js
+++ src/components/BuildDash/BuilderTable.js
@@ -5,7 +5,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import TableCell from '@material-ui/core/TableCell';
-import TableBody from "@material-ui/core/TableBody";
+//import TableBody from "@material-ui/core/TableBody";
+import Card from '@material-ui/core/Card';
 //import BikeTableRow from "./BikeTableRow";
 import "./BuildDash.css";
 
@@ -17,7 +18,7 @@ class BuilderTable extends Component {
   render() {
 
     return (
-      
+      <Card>
       <Paper>
        
         <Table className="main-table">
@@ -37,6 +38,7 @@ class BuilderTable extends Component {
    </TableBody> */}
         </Table>
       </Paper>
+      </Card>
     );
   }
 }
