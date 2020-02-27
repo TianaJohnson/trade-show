cbf9ef0663bfd67fc61c3ceb5ffb3c04b02c7298
Tiana Johnson <tiana.marie013@gmail.com>
2020-02-26 21:12:01 -0700

    postgres issues unrealted to the code, hopefully fixed

--- src/redux/sagas/showSaga.js
+++ src/redux/sagas/showSaga.js
@@ -24,24 +24,24 @@ function* addShow(action) {
     alert('Unabale to fetch customers from server', error);
   }
 }
-// attempt this again
-function* deleteShowInfo(action) {
-  const show = action.payload;
-  console.log('In saga archive delete', show);
-  try {
-      yield axios.delete(`/api/show/${show}`);
-      let nextAction = { type: 'FETCH_SHOW' };
-      yield put(nextAction);
-  }catch(error){
-    console.log('Unable to delete trade show info', error);
-    alert('Unable to delete trade show information', error);
-  }
-}
+// // attempt this again
+// function* deleteShowInfo(action) {
+//   const show = action.payload;
+//   console.log('In saga archive delete', show);
+//   try {
+//       yield axios.delete(`/api/show/${show}`);
+//       let nextAction = { type: 'FETCH_SHOW' };
+//       yield put(nextAction);
+//   }catch(error){
+//     console.log('Unable to delete trade show info', error);
+//     alert('Unable to delete trade show information', error);
+//   }
+// }
   
   function* showIntakesaga() {
     yield takeLatest('ADD_SHOW', addShow);
     yield takeLatest('FETCH_SHOW', fetchShow);
-    yield takeLatest('DELETE_SHOW_INFO', deleteShowInfo);
+   // yield takeLatest('DELETE_SHOW_INFO', deleteShowInfo);
     
   }
   export default showIntakesaga;
