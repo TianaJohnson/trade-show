c766890312e968aefb82b2dc5b5455efaabd1c57
Tiana <Tiana.marie013@gmail.com>
2020-03-31 19:04:58 -0600

    ...

--- src/components/Page/page.js
+++ src/components/Page/page.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import './Page.css';
-// import styled from 'styled-components';
 
-// const Card = styled.div`
-//     .page-card {
-//         height: 1000px
-//     }`;
 
 class Page extends Component {
     render() {
