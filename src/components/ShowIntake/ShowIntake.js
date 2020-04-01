600e806d63752256594b6923818d6b126c5a9d09
Tiana <Tiana.marie013@gmail.com>
2020-03-31 19:02:07 -0600

    adding css to show intake

--- src/components/ShowIntake/ShowIntake.js
+++ src/components/ShowIntake/ShowIntake.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Page from './../Page/Page.js';
 
 class ShowIntake extends Component {
 
