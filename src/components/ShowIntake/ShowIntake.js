760649c100f72dce92dd89db6842287042f9c7e7
Tiana <Tiana.marie013@gmail.com>
2020-03-31 19:03:53 -0600

    height

--- src/components/ShowIntake/ShowIntake.js
+++ src/components/ShowIntake/ShowIntake.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import './ShowIntake.css';
 
 class ShowIntake extends Component {
 
